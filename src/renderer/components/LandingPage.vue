8ddf352241452c01ba1c23c0d00ad28fc1b21743
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2019-12-21 23:32:22 +0600

    Initial commit

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Hello World</h1>
+</template>
+
+<script>
+import SystemInformation from "./LandingPage/SystemInformation";
+
+export default {
+    name: "landing-page",
+    components: { SystemInformation },
+    methods: {
+        open(link) {
+            this.$electron.shell.openExternal(link);
+        }
+    }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro");
+</style>
